8238c92be6ae35a879f1deaec5b94a3cf5dbd986
youshoubianer <you_shoubian@163.com>
2016-11-13 16:02:32 +0800

    casperjs login

---
+++ utils/QzoneLogin.js
@@ -0,0 +1,38 @@
+//qq空间模拟登录
+//casperjs
+
+var qq = ''
+var pwd = ''
+
+var url = 'http://qzone.qq.com'
+var casper = require('casper').create(); 
+casper.start(url);
+
+casper.withFrame('login_frame', function() {
+  this.open(this.getCurrentUrl())
+})
+
+casper.waitForSelector('#switcher_plogin')
+casper.thenClick('#switcher_plogin')
+casper.waitForSelector('#loginform')
+casper.then(function(){
+  this.fillSelectors('#loginform', {
+       'input[name="u"]': qq,
+       'input[name="p"]': pwd
+   }, false);
+})
+casper.waitForSelector('#login_button')
+casper.thenClick('#login_button')
+casper.wait(1000,function(){
+  this.echo(this.getTitle());
+})
+
+//get cookie
+casper.then(function() {
+    var cookies = phantom.cookies;
+    this.echo("cookie.length = " + cookies.length);
+    for (var i in cookies) {
+        this.echo(cookies[i].name + "=" +  cookies[i].value);
+    }
+});
+casper.run();
